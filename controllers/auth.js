970e118a9372f5c1a593b90dd79b5865d9515ce1
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-19 13:26:34 +0100

    Fix - Added password hashing with mongose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,21 +5,7 @@ import { BadRequest } from "../errors";
 import bcryptjs from "bcryptjs";
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  //   Optional - We are using moongose valiation
-  //   if (!name || !email || !password) {
-  //     throw new BadRequest("Please provide name,email and password");
-  //   }
-
-  //Hashing password
-
-  const salt = await bcryptjs.genSalt(10);
-
-  const hashedPassword = await bcryptjs.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json(user);
 };
 
