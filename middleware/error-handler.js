638f119540f30110e417c8d4bcde9ad87d4fc3bc
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-22 16:31:19 +0100

    Refactoring - Error handling for duplicate errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,10 +2,24 @@ import { StatusCodes } from "http-status-codes";
 import { CustomAPIError } from "../errors/index.js";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let CustomError = {
+    //default error object
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong, PLease try again later",
+  };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ err });
+
+  if (err.code && err.code === 11000) {
+    CustomError.message = `${Object.keys(
+      err.keyValue
+    )} has been used already. Please choose another email address`;
+    CustomError.statusCode = 400;
+  }
+
+  return res.status(CustomError.statusCode).json({ msg: CustomError.message });
 };
 
 export default errorHandlerMiddleware;
