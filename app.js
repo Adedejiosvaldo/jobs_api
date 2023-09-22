fe0914c9bbf9b9741c54995a7c4db22b61bc0099
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-22 17:14:52 +0100

    Security - Added security middleware

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// packages
 import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
 import express from "express";
+import xss from "xss-clean";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
+// imports
 import jobsRouter from "./routes/jobs.js";
 import authRouter from "./routes/auth.js";
 import notFound from "./middleware/not-found.js";
@@ -11,14 +17,30 @@ import connectDB from "./db/connect.js";
 import authentificationMiddleWare from "./middleware/authentication.js";
 
 const app = express();
-// middleware
-app.use(express.json());
+
+//routers
 app.use("/api/v1/jobs/", authentificationMiddleWare, jobsRouter);
 app.use("/api/v1/auth/", authRouter);
 
+// middleware
+app.use(express.json());
 app.use(errorHandlerMiddleware);
 app.use(notFound);
+// seccurity middleware
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, //Limit each IP to 100 requests per windowMS
+  })
+);
+app.use(helmet);
+app.use(cors);
+app.use(xss);
+
 const port = process.env.PORT || 5000;
+
+//Main express app
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
