203687d7b972bb480ac12ce6a305c1fadb244e51
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-22 16:54:08 +0100

    Refactoring - Validation Error Handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -8,17 +8,26 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     message: err.message || "Something went wrong, PLease try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  //   if (err instanceof CustomAPIError) {
+  //     return res.status(err.statusCode).json({ msg: err.message });
+  //   }
+
+  if (err.name === "ValidationError") {
+    CustomError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+
+    CustomError.statusCode = 400;
   }
 
   if (err.code && err.code === 11000) {
     CustomError.message = `${Object.keys(
       err.keyValue
-    )} has been used already. Please choose another email address`;
+    )} has been used already. Please choose another value`;
     CustomError.statusCode = 400;
   }
 
+  //   return res.status(CustomError.statusCode).json({ err });
   return res.status(CustomError.statusCode).json({ msg: CustomError.message });
 };
 
