4710f82b1ac86e756f52b70bb5551b7cc4f67c2a
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-29 16:25:40 +0100

    Documentation - Added swaggerUI

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ import errorHandlerMiddleware from "./middleware/error-handler.js";
 import connectDB from "./db/connect.js";
 import authentificationMiddleWare from "./middleware/authentication.js";
 
+//Swagger
+import SwaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
+const swagerDocument = YAML.load("./swagger.yaml");
 const app = express();
 
 app.use(express.json());
@@ -41,8 +45,10 @@ const port = process.env.PORT || 5000;
 app.use("/api/v1/jobs/", authentificationMiddleWare, jobsRouter);
 app.use("/api/v1/auth/", authRouter);
 
+app.use("api-docs", SwaggerUI.serve, SwaggerUI.setup(swagerDocument));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Homepage</h1>");
+  res.send('<h1>Jobs Api</h1><a href="/api-doc">Documentation</a>' );
 });
 
 const start = async () => {
